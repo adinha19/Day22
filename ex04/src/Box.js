8bf6c198c2a091ac09bf988a172a05af1dcf64d6
Adin Had <adinhadzovic1993@gmail.com>
2021-03-30 00:57:39 +0200

    sve radi...hmm...

---
+++ ex04/src/Box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    render() {
+        return (
+            <div className="Box">
+                <h2>This is a Box Component!</h2>
+            </div>
+        )
+    }
+}
+
+export default Box;
---
+++ ex09/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Flower from './Flower'
+
+function App() {
+  return (
+    <div className="App">
+      <Flower color="red"/>
+    </div>
+  );
+}
+
+export default App;
